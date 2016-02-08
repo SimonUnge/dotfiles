bab21fa98969e20930bda3bd21b07b67cdb2fe14
Simon Unge <su@tail-f.com>
2016-02-08 14:40:13 +0100

    new

---
+++ .emacs.d/clispec/clispec-mode.el
@@ -0,0 +1,13 @@
+;;; clispec-mode.el
+
+;; Author: Simon Unge
+ ;;; Code:
+
+ ;;;###autoload
+(define-derived-mode clispec-mode nxml-mode
+  "Cli-spec mode"
+  "A major mode for editing cli-spec files."
+  )
+
+(provide 'clispec-mode)
+ ;;;
