2377cf657709b9eb9a1827016cddba0bd1dcaac8
Simon Unge <sunge@cisco.com>
2020-02-28 11:03:48 -0800

    new emacs ignore

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -119,3 +119,6 @@
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(setq pe/omit-gitignore 1)
+(auto-fill-mode -1)
