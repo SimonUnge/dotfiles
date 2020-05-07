d78f0f5617dee2891cf17b4d683c8e8c933fd35b
Simon Unge <sunge@cisco.com>
2020-05-07 14:02:36 -0700

    I dont know what I did

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -81,10 +81,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(Man-notify-method (quote pushy))
- '(org-agenda-files nil t)
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (gruvbox-theme iedit tabbar helm project-explorer rainbow-delimiters projectile rainbow-identifiers rainbow-mode plantuml-mode docbook-snippets docbook buffer-expose groovy-mode elixir-mode elixir-yasnippets py-autopep8 idomenu pylint erlang yaml-mode mew))))
+    (magit gruvbox-theme iedit tabbar helm project-explorer rainbow-delimiters projectile rainbow-identifiers rainbow-mode plantuml-mode docbook-snippets docbook buffer-expose groovy-mode elixir-mode elixir-yasnippets py-autopep8 idomenu pylint erlang yaml-mode mew))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
